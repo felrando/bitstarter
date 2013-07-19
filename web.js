794edeacdb0664fea7f453094901832686705d88
felrando <felice.rando@gmail.com>
2013-07-19 05:28:45 +0000

    updated web.js to point to index.html

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var buffer = new Buffer('index.html');
-    response.send(fs.readFileSync(buffer).toString());
+    var buffer = new Buffer(128);
+    buffer = fs.readFileSync("index.html");
+    response.send(buf.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
