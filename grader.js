4a5810c9aa8dd9a4ba71a5dcdc6b022e3ce51f68
felrando <felice.rando@gmail.com>
2013-07-22 06:10:21 +0000

    adding grader.js and assorted

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(htmlfile, checksfile) {
+    var instr = htmlfile.toString();
+    $ = cheerio.load(instr);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +82,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url', URL_DEFAULT)
         .parse(process.argv);
+    
+    if(program.url != URL_DEFAULT) {
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error){
+		console.log("URL not found");
+		process.exit(1);
+	} else {
+	    console.log("checking " + program.url);
+	    checkUrl(result, program.checks);
+	    }
+	});
+    } else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
